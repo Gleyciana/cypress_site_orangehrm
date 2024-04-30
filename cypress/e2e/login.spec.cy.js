36fe65068ff4db84493683292add03bcda6fa2b1
Gleyciana Cameplo <gleycianacsombra@gmail.com>
2024-04-30 15:26:55 -0300

    added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,38 +1,45 @@
 /// <reference types="cypress" />
 
 describe('Login-Orange HRM Tests', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: ".oxd-alert-content > .oxd-text"
+
+  }
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
   })
   it('Login - Invalid username and password', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Teste')
-    cy.get("[name='password']").type
-  ('teste123')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials')
+    cy.get(selectorList.usernameField).type('Teste')
+    cy.get(selectorList.passwordField).type('teste123')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
   })
   it('Login - Invalid username', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Gleyci')
-    cy.get("[name='password']").type
-  ('admin123')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials')
+    cy.get(selectorList.usernameField).type('Gleyci')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
 
   })
   it('Login - Invalid password', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type
-  ('teste123')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials')
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('teste123')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
   })
 })
 
