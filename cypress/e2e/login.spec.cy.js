70f5314fe5758197e644702059edd01b553e91f6
Gleyciana Cameplo <gleycianacsombra@gmail.com>
2024-05-01 14:14:48 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import userData from '../fixtures/users/userData.json';
+
 describe('Login-Orange HRM Tests', () => {
 
   const selectorList = {
@@ -12,44 +14,32 @@ describe('Login-Orange HRM Tests', () => {
 
   }
 
-  const userData = {
-    userSucess:{
-      username:'Admin',
-      password:'admin123'
-    },
-    userFail:{
-      username:'Teste',
-      password:'teste123'
-
-    }
-  }
-
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorList.passwordField).type(userData.userSucess.password)
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
   })
   it('Login - Invalid username and password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
   })
   it('Login - Invalid username', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userSucess.password)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
 
   })
   it('Login - Invalid password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSucess.username)
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
