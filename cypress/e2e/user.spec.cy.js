2288150573ad7da7934c67b96d0e27e45ad316a2
Gleyciana Cameplo <gleycianacsombra@gmail.com>
2024-05-02 11:59:40 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,35 +1,40 @@
 /// <reference types="cypress" />
 
-import userData from '../fixtures/users/userData.json';
+import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Login-Orange HRM Tests', () => {
 
   const selectorList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid:".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert-content > .oxd-text",
-    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']",
+    
+   
     firstNameField:"[name='firstName']",
     middleNameField:"[name='middleName']",
     lastNameField:"[name='lastName']",
     genericField:".oxd-input--active",
     dateField:"[placeholder='yyyy-dd-mm']",
+    genericCombobox:".oxd-select-text--arrow",
+    secondItemCombobox:".oxd-select-dropdown > :nth-child(27)",
+    thirdItemCombobox:".oxd-select-dropdown > :nth-child(3)",
     dateCloseButton:".--close",
     submitButton:"[type='submit']"
 
   }
 
   it.only('User Info Update', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid)
-    cy.get(selectorList.myInfoButton).click()
+    loginPage.acessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username,userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+    
     cy.get(selectorList.firstNameField).clear().type('Gleyciana')
     cy.get(selectorList.middleNameField).clear().type('Campelo')
     cy.get(selectorList.lastNameField).clear().type('Sombra')
@@ -38,33 +43,15 @@ describe('Login-Orange HRM Tests', () => {
     cy.get(selectorList.dateCloseButton).click()
     cy.get(selectorList.genericField).eq(7).clear().type('1989-03-23')
     cy.get(selectorList.dateCloseButton).click()
-    cy.get(selectorList.submitButton).eq(0).click()
+    cy.get(selectorList.submitButton).eq(0).click({ force: true})
     cy.get('.oxd-toast-content').should('contain','Successfully Updated')
-  })
+    cy.get('.oxd-toast-close')
 
-
-
-  it('Login - Invalid username and password', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
-  })
-  it('Login - Invalid username', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
-
-  })
-  it('Login - Invalid password', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert).contains('Invalid credentials')
+    cy.get(selectorList.genericCombobox).eq(0).click({ force: true})
+    cy.get(selectorList.secondItemCombobox).click()
+    cy.get(selectorList.genericCombobox).eq(1).click({ force: true})
+    cy.get(selectorList.thirdItemCombobox).click()
+    
   })
 })
 
---
+++ cypress/pages/menuPage.js
@@ -0,0 +1,18 @@
+class MenuPage{
+
+    selectorList(){
+        const selector= {
+            myInfoButton:"[href='/web/index.php/pim/viewMyDetails']",
+
+        }
+
+        return selector
+    }
+
+    accessMyInfo(){
+        cy.get(this.selectorList().myInfoButton).click()
+
+    }
+}
+
+export default MenuPage
